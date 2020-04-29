e2265fc63a6d1e33bc91706ebdb70c01df0bd8de
ibtihel <ibtihel.latrech@ensi-uma.tn>
2020-04-29 12:33:05 +0200

    add return user to login

--- ecommerce/src/main/java/com/pcd/ecommerce/controller/LoginController.java
+++ ecommerce/src/main/java/com/pcd/ecommerce/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.pcd.ecommerce.controller;
 
+import com.pcd.ecommerce.model.MyUserDetails;
+import com.pcd.ecommerce.model.Product;
 import com.pcd.ecommerce.model.User;
+import com.pcd.ecommerce.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
 
 @RestController
@@ -21,29 +27,32 @@ import static org.springframework.security.web.context.HttpSessionSecurityContex
 public class LoginController {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     AuthenticationManager authManager;
 
     @GetMapping
-    public String greet(){
+    public String greet() {
         return "welcome to login";
     }
 
     @PutMapping
-    public String greetings(){
+    public String greetings() {
         return "you tried updating";
     }
 
     @PostMapping
-    public Authentication login(@RequestBody User user, HttpServletRequest req) {
+    public User login(@RequestBody User user, HttpServletRequest req) {
         UsernamePasswordAuthenticationToken authReq
-                = new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
+                = new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword());
         Authentication auth = authManager.authenticate(authReq);
 
         SecurityContext sc = SecurityContextHolder.getContext();
         sc.setAuthentication(auth);
         HttpSession session = req.getSession(true);
         session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
-        return auth;
+        return this.userRepository.findByUserName(user.getUserName()).get();
     }
 
 
